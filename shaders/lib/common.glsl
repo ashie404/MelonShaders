de233ddb4189aeb9c146388c8491314ece218a3c
thejshua <joyesh708@gmail.com>
2020-03-27 14:56:51 -0500

    LUT tweaks and new default color grading profile

--- shaders/lib/common.glsl
+++ shaders/lib/common.glsl
@@ -39,13 +39,13 @@ Lightmap getLightmapSample(in vec2 coord) {
 void dayNightCalc(out float night, out vec3 lightVec, out vec3 lightCol, out vec3 skyCol) {
     if (worldTime >= 23645 || worldTime <= 12400) {
         lightVec = normalize(sunPosition);
-        lightCol = vec3(1.65);
-        skyCol = vec3(0.012, 0.015, 0.023);
+        lightCol = vec3(4.5);
+        skyCol = vec3(0.022, 0.025, 0.06);
         night = 0;
     } else if (worldTime >= 12820 && worldTime <= 23200) {
         lightVec = normalize(moonPosition);
         lightCol = vec3(0.5);
-        skyCol = vec3(0.0012, 0.0015, 0.0023);
+        skyCol = vec3(0.0005, 0.0010, 0.002);
         night = 1;
     } else {
         float transition = smoothstep(50,820,abs(abs(float(worldTime)-17990)-5220));
@@ -55,7 +55,7 @@ void dayNightCalc(out float night, out vec3 lightVec, out vec3 lightCol, out vec
             lightVec = normalize(sunPosition);
         }
         night = transition;
-        lightCol = mix(vec3(0.5), vec3(1.65), transition);
+        lightCol = mix(vec3(0.5), vec3(5), transition);
         skyCol = mix(vec3(0.0012, 0.0015, 0.0023), vec3(0.012, 0.015, 0.023), transition);
     }
 }
