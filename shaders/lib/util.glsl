797e10e1f0641bd987a1f7882ff6048341a569e3
thejshua <joyesh708@gmail.com>
2020-06-10 17:02:47 -0500

    fix color banding when looking at the sky underwater

--- shaders/lib/util.glsl
+++ shaders/lib/util.glsl
@@ -7,6 +7,7 @@
 const int colortex0Format = RGBA16F;
 const int colortex1Format = RGBA16F;
 const int colortex4Format = RGBA16F;
+const int colortex5Format = RGBA16F;
 const int colortex6Format = RGBA16F;
 const bool colortex6Clear = false;
 */
