4fcf422a3a345d49908b450e511b87e7ff4bc5f1
thejshua <joyesh708@gmail.com>
2020-06-11 10:36:26 -0500

    fix weird reflection bugs with translucents

--- shaders/lib/reflection.glsl
+++ shaders/lib/reflection.glsl
@@ -5,7 +5,7 @@
 
 vec4 reflection(vec3 viewPos, vec3 normal, float dither, sampler2D reflectionTex) {
 	vec4 outColor = vec4(0.0);
-	vec4 rtPos = raytrace(depthtex0, viewPos, normal, dither, 4.0, 1.0, 0.1, 2.0);
+	vec4 rtPos = raytrace(depthtex1, viewPos, normal, dither, 4.0, 1.0, 0.1, 2.0);
 	if (rtPos.w <= 100.0 && rtPos.x >= 0.0 && rtPos.x <= 1.0 && rtPos.y >= 0.0 && rtPos.y <= 1.0) {
 		outColor = texture2D(reflectionTex, rtPos.xy);
 	}
