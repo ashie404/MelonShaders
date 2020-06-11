07cd6d74e259fa5692fcbae2e322da6dd59acf2a
thejshua <joyesh708@gmail.com>
2020-06-11 12:02:31 -0500

    make moon a bit bigger

--- shaders/lib/atmosphere.glsl
+++ shaders/lib/atmosphere.glsl
@@ -239,7 +239,7 @@ vec3 getSkyColor(vec3 worldPos, vec3 viewVec, vec3 sunVec, vec3 moonVec, float a
     #endif
 
     skyColor += mix(vec3(0.0), mix(vec3(0.0), calculateSunSpot(viewVec, sunVec, CELESTIAL_RADIUS, false).rgb, clamp01(1.0-cloudShape)), clamp01(worldPos.y*64.0))*skyColor;
-    skyColor += mix(vec3(0.0), calculateSunSpot(viewVec, moonVec, CELESTIAL_RADIUS, true).rgb, clamp01(1.0-cloudShape));
+    skyColor += mix(vec3(0.0), calculateSunSpot(viewVec, moonVec, CELESTIAL_RADIUS+0.1, true).rgb, clamp01(1.0-cloudShape));
 
     #ifdef STARS
 
