ed75a07424aeedacfb6b7714d841cbf05bff8da7
thejshua <joyesh708@gmail.com>
2020-06-15 10:55:27 -0500

    waving plant shadows

--- shaders/program/gbuffers/shadow.glsl
+++ shaders/program/gbuffers/shadow.glsl
@@ -46,19 +46,33 @@ out vec4 color;
 out float isWater;
 
 // uniforms
-uniform mat4 gbufferModelViewInverse;
 uniform mat4 shadowModelViewInverse;
+uniform mat4 gbufferModelViewInverse;
+uniform mat4 shadowModelView;
+uniform vec3 cameraPosition;
+uniform float frameTimeCounter;
 
 attribute vec3 mc_Entity;
+attribute vec3 mc_midTexCoord;
 
 // includes
 
+#include "/lib/noise.glsl"
 #include "/lib/distort.glsl"
 
 void main() {
 	texcoord = (gl_TextureMatrix[0] * gl_MultiTexCoord0).xy;
 	color = gl_Color;
-	gl_Position = ftransform();
+
+	vec4 position = shadowModelViewInverse * gl_ModelViewMatrix * gl_Vertex;
+
+	#ifdef WIND
+    if ((mc_Entity.x == 20.0 && gl_MultiTexCoord0.t < mc_midTexCoord.t) || mc_Entity.x == 23) {
+        position.xz += sin(frameTimeCounter*cellular(position.xyz + cameraPosition)*4)/32;
+    }
+    #endif
+
+	gl_Position = gl_ProjectionMatrix * shadowModelView * position;
 	gl_Position.xyz = distort(gl_Position.xyz);
 	if (mc_Entity.x == 8.0) {
 		isWater = 1.0;
