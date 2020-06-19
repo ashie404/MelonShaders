a6a180c12448ed1e06a6c7e4098e9ce392e9910f
thejshua <joyesh708@gmail.com>
2020-06-19 17:40:02 -0500

    a

--- shaders/lib/settings.glsl
+++ shaders/lib/settings.glsl
@@ -69,9 +69,9 @@
 
 // blocklight color
 #define BLOCKLIGHT_R 1.0 // [0.0 0.1 0.2 0.25 0.3 0.4 0.5 0.6 0.7 0.75 0.8 0.9 1.0]
-#define BLOCKLIGHT_G 0.5 // [0.0 0.1 0.2 0.25 0.3 0.4 0.5 0.6 0.7 0.75 0.8 0.9 1.0]
-#define BLOCKLIGHT_B 0.25 // [0.0 0.1 0.2 0.25 0.3 0.4 0.5 0.6 0.7 0.75 0.8 0.9 1.0]
-#define BLOCKLIGHT_I 0.75 // Intensity of blocklight. [0.0 0.1 0.2 0.25 0.3 0.4 0.5 0.6 0.7 0.75 0.8 0.9 1.0]
+#define BLOCKLIGHT_G 0.8 // [0.0 0.1 0.2 0.25 0.3 0.4 0.5 0.6 0.7 0.75 0.8 0.9 1.0]
+#define BLOCKLIGHT_B 0.7 // [0.0 0.1 0.2 0.25 0.3 0.4 0.5 0.6 0.7 0.75 0.8 0.9 1.0]
+#define BLOCKLIGHT_I 1.0 // Intensity of blocklight. [0.0 0.1 0.2 0.25 0.3 0.4 0.5 0.6 0.7 0.75 0.8 0.9 1.0]
 
 #define EMISSIVE_MAP 0 // Emissive map setting. [0 1 2]
 #define EMISSIVE_MAP_STRENGTH 1.0 // Strength of emissive maps. [0.25 0.5 0.75 1.0 1.25 1.5 1.75 2.0 2.5 3.0 3.5 4.0 4.5 5.0 6.0 7.0 8.0 9.0 10.0]
