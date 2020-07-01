01b9425d22f5d1b3bdef642f0ea29f5eb11722ea
thejshua <joyesh708@gmail.com>
2020-07-01 16:05:34 -0500

    maybe that was a bit too aggressive

--- shaders/lib/settings.glsl
+++ shaders/lib/settings.glsl
@@ -26,9 +26,9 @@
 #define COLOR_AP1 // Whether to use the AP1 color space or not. Makes colors look more natural.
 
 // shadow settings
-#define SHADOW_SOFTNESS 1.0 // How soft the shadows should be. [1.0 1.25 1.5 1.75 2.0 2.25 2.5 2.75 3.0 3.5 4.0 4.5 5.0 6.0 7.0 8.0 9.0 10.0]
+#define SHADOW_SOFTNESS 1.0 // How soft the shadows should be. [0.25 0.5 0.75 1.0 1.25 1.5 1.75 2.0 2.25 2.5 2.75 3.0 3.5 4.0 4.5 5.0 6.0 7.0 8.0 9.0 10.0]
 
-#define SHADOW_BIAS 0.000025
+#define SHADOW_BIAS 0.00005
 
 // sky settings
 #define CELESTIAL_RADIUS 0.25 // Radius of celestial bodies (the sun and moon). [ 0.1 0.25 0.3 0.4 0.5 0.6 0.7 ]
