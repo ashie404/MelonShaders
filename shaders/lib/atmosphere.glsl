2f3c202e2a984f5006421a0cd6599ce097e25a19
thejshua <joyesh708@gmail.com>
2020-07-03 08:19:29 -0500

    atmosphere tweaks

--- shaders/lib/atmosphere.glsl
+++ shaders/lib/atmosphere.glsl
@@ -149,7 +149,7 @@ vec3 getSkyColor(vec3 worldPos, vec3 viewVec, vec3 sunVec, vec3 moonVec, float a
     if (atmosphereOnly) {
         skyPos.y = max(skyPos.y, 10.0);
     } else {
-        skyPos.y = max(skyPos.y, 0.0);
+        skyPos.y = max(skyPos.y, 2.5);
     }
 
     vec3 skyColor = vec3(0.0);
@@ -162,7 +162,7 @@ vec3 getSkyColor(vec3 worldPos, vec3 viewVec, vec3 sunVec, vec3 moonVec, float a
         6371e3,                         // radius of the planet in meters
         6471e3,                         // radius of the atmosphere in meters
         vec3(5.5e-6, 13.0e-6, 22.4e-6), // Rayleigh scattering coefficient
-        30.2e-6,                          // Mie scattering coefficient
+        30.2e-6*2.5,                          // Mie scattering coefficient
         8e3,                            // Rayleigh scale height
         1.2e3,                          // Mie scale height
         0.758,                           // Mie preferred scattering direction
