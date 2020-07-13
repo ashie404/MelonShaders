3bf6055e048b56e6d0f73aa526c7ffb9c185c227
juniebyte <joyesh708@gmail.com>
2020-07-12 20:31:39 -0500

    h

--- shaders/lib/fragmentUtil.glsl
+++ shaders/lib/fragmentUtil.glsl
@@ -14,11 +14,17 @@ struct Fragment {
 
 Fragment getFragment(in vec2 coord) {
     Fragment f;
-    f.albedo = texture2D(colortex0, coord);
-    f.lightmap = texture2D(colortex1, coord).xy;
-    f.matMask = int(texture2D(colortex1, coord).a+0.5);
-    f.normal = texture2D(colortex2, coord).xyz * 2.0 - 1.0;
-    f.specular = texture2D(colortex3, coord);
+
+    vec4 tex0 = texture2D(colortex0, coord);
+    vec4 tex1 = texture2D(colortex1, coord);
+    vec4 tex2 = texture2D(colortex2, coord);
+    vec4 tex3 = texture2D(colortex3, coord);
+
+    f.albedo = tex0;
+    f.lightmap = tex1.xy;
+    f.matMask = int(tex1.a+0.5);
+    f.normal = tex2.xyz * 2.0 - 1.0;
+    f.specular = tex3;
     f.coord = coord;
     return f;
 }
