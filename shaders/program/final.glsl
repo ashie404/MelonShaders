e789cb53d2438fa9172a2057bfffeb7966d688c6
juniebyte <joyesh708@gmail.com>
2020-07-15 07:14:09 -0500

    re-add debug mode

--- shaders/program/final.glsl
+++ shaders/program/final.glsl
@@ -111,7 +111,11 @@ void main() {
     color = lookup(color);
     #endif
 
-    screenOut = vec4(color, 1.0);
+    if (DEBUG_MODE == 0) screenOut = vec4(color, 1.0);
+    else if (DEBUG_MODE == 1) screenOut = vec4(decodeLightmaps(texture2D(colortex1, texcoord).x), 0.0, 1.0);
+    else if (DEBUG_MODE == 2) screenOut = vec4(decodeNormals(texture2D(colortex1, texcoord).y), 1.0);
+    else if (DEBUG_MODE == 3) screenOut = texture2D(colortex2, texcoord);
+    else if (DEBUG_MODE == 4) screenOut = texture2D(colortex4, texcoord);
 }
 
 #endif
