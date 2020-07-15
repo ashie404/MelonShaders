e23d6f7d3d3cc232b912b55a7f73acdadbafc08c
juniebyte <joyesh708@gmail.com>
2020-07-15 10:08:48 -0500

    high quality reflections option

--- shaders/lib/reflection.glsl
+++ shaders/lib/reflection.glsl
@@ -6,7 +6,11 @@
 vec4 reflection(vec3 viewPos, vec3 normal, float dither, sampler2D reflectionTex) {
 	vec4 outColor = vec4(0.0);
 	float fresnel = clamp01(fresnel(0.2, 0.1, 1.0, viewPos, normal)+0.2);
+	#ifdef HQ_REFLECTIONS
+	vec4 rtPos = raytrace(depthtex1, viewPos, reflect(normalize(viewPos), normalize(normal)), dither, 4.0, 1.0, 0.1, 1.0);
+	#else
 	vec4 rtPos = raytrace(depthtex1, viewPos, reflect(normalize(viewPos), normalize(normal)), dither, 4.0, 1.0, 0.1, 2.0);
+	#endif
 	if (rtPos.w <= 100.0 && rtPos.x >= 0.0 && rtPos.x <= 1.0 && rtPos.y >= 0.0 && rtPos.y <= 1.0 && rtPos.z < 1.0 - 1e-5) {
 		outColor = texture2D(reflectionTex, rtPos.xy);
 	}
@@ -15,7 +19,11 @@ vec4 reflection(vec3 viewPos, vec3 normal, float dither, sampler2D reflectionTex
 
 vec4 roughReflection(vec3 viewPos, vec3 normal, float dither, float roughness, sampler2D reflectionTex) {
 	vec4 outColor = vec4(0.0);
+	#ifdef HQ_REFLECTIONS
+	vec4 rtPos = raytrace(depthtex1, viewPos, reflect(normalize(viewPos), normalize(normal)), dither, 4.0, 1.0, 0.1, 1.0);
+	#else
 	vec4 rtPos = raytrace(depthtex1, viewPos, reflect(normalize(viewPos), normalize(normal)), dither, 4.0, 1.0, 0.1, 2.0);
+	#endif
 	float lod = roughness*12.0;
 	if (rtPos.w <= 100.0 && rtPos.x >= 0.0 && rtPos.x <= 1.0 && rtPos.y >= 0.0 && rtPos.y <= 1.0 && rtPos.z < 1.0 - 1e-5) {
 		outColor = texture2DLod(reflectionTex, rtPos.xy, lod);
