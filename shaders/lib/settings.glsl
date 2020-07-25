1182691e3504afcd8a99bed4a42e8a33ea588e32
juniebyte <joyesh708@gmail.com>
2020-07-24 22:49:36 -0500

    yes more shadow distance values

--- shaders/lib/settings.glsl
+++ shaders/lib/settings.glsl
@@ -28,7 +28,7 @@
 
 const int noiseTextureResolution = 512;
 const int shadowMapResolution = 2048; // [1024 2048 4096 8192]
-const int shadowDistance = 128; // [128 256 512 1024]
+const int shadowDistance = 128; // [128 256 512 1024 2048 4096]
 const float sunPathRotation = -40.0; // [-40.0 -35.0 -30.0 -25.0 -20.0 -15.0 -10.0 10.0 15.0 20.0 25.0 30.0 35.0 40.0]
 
 // lighting settings
