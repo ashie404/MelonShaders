fad42e088e0db025215ac4b72e9562742da96fd0
juniebyte <joyesh708@gmail.com>
2020-07-26 10:33:29 -0500

    remove hand from dof calculation

--- shaders/program/composite/stage_2.glsl
+++ shaders/program/composite/stage_2.glsl
@@ -29,6 +29,7 @@ uniform sampler2D colortex2;
 
 uniform sampler2D depthtex0;
 uniform sampler2D depthtex1;
+uniform sampler2D depthtex2;
 
 uniform float viewWidth;
 uniform float viewHeight;
@@ -41,28 +42,30 @@ void main() {
 
     float currentDepth = texture2D(depthtex0, texcoord).r;
 
-    vec2 oneTexel = 1.0 / vec2(viewWidth, viewHeight);
+    if (currentDepth >= texture2D(depthtex2, texcoord).r) {
+        vec2 oneTexel = 1.0 / vec2(viewWidth, viewHeight);
 
-    // distance blur
-    if (currentDepth >= centerDepthSmooth) {
-        vec3 blurred = vec3(0.0);
-        float blurSize = clamp((currentDepth-centerDepthSmooth)*(256.0*APERTURE), 0.0, (12.0*APERTURE));
-        for (int i = 0; i <= 8; i++) {
-            vec2 offset = vogelDiskSample(i, 8, interleavedGradientNoise(gl_FragCoord.xy))*oneTexel*blurSize;
-            blurred += texture2D(colortex0, texcoord+offset).rgb;
+        // distance blur
+        if (currentDepth >= centerDepthSmooth) {
+            vec3 blurred = vec3(0.0);
+            float blurSize = clamp((currentDepth-centerDepthSmooth)*(256.0*APERTURE), 0.0, (12.0*APERTURE));
+            for (int i = 0; i <= 8; i++) {
+                vec2 offset = vogelDiskSample(i, 8, interleavedGradientNoise(gl_FragCoord.xy))*oneTexel*blurSize;
+                blurred += texture2D(colortex0, texcoord+offset).rgb;
+            }
+            color = blurred / 8.0;
         }
-        color = blurred / 8.0;
-    }
 
-    // close blur
-    if (currentDepth <= centerDepthSmooth) {
-        vec3 blurred = vec3(0.0);
-        float blurSize = clamp((centerDepthSmooth-currentDepth)*(256.0*APERTURE), 0.0, (12.0*APERTURE));
-        for (int i = 0; i <= 8; i++) {
-            vec2 offset = vogelDiskSample(i, 8, interleavedGradientNoise(gl_FragCoord.xy))*oneTexel*blurSize;
-            blurred += texture2D(colortex0, texcoord+offset).rgb;
+        // close blur
+        if (currentDepth <= centerDepthSmooth) {
+            vec3 blurred = vec3(0.0);
+            float blurSize = clamp((centerDepthSmooth-currentDepth)*(256.0*APERTURE), 0.0, (12.0*APERTURE));
+            for (int i = 0; i <= 8; i++) {
+                vec2 offset = vogelDiskSample(i, 8, interleavedGradientNoise(gl_FragCoord.xy))*oneTexel*blurSize;
+                blurred += texture2D(colortex0, texcoord+offset).rgb;
+            }
+            color = blurred / 8.0;
         }
-        color = blurred / 8.0;
     }
 
     #endif
