168bcdceb519c14bd9e89e05a2bc37d4c050d29c
juniebyte <joyesh708@gmail.com>
2020-08-04 16:48:21 -0500

    fix bright line on edge of reflection with microfacet off

--- shaders/lib/fragment/reflection.glsl
+++ shaders/lib/fragment/reflection.glsl
@@ -67,7 +67,7 @@ vec4 reflection(vec3 viewPos, vec3 normal, float dither, sampler2D reflectionTex
 	vec4 outColor = vec4(0.0);
 	vec4 rtPos = raytrace(depthtex0, viewPos, reflect(normalize(viewPos), normalize(normal)), dither, 4.0, 1.0, 0.1, 1.5);
 	if (rtPos.w <= 100.0 && rtPos.x >= 0.0 && rtPos.x <= 1.0 && rtPos.y >= 0.0 && rtPos.y <= 1.0 && rtPos.z < 1.0 - 1e-5) {
-		outColor = texture2D(reflectionTex, rtPos.xy);
+		outColor = texture2DLod(reflectionTex, rtPos.xy, 0.0);
 	}
 	return clamp(outColor, 0.0, 1000.0);
 }
