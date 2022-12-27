9b4090046e4dafdfdb1019e4460744caef362b29
June <22226164+juniebyte@users.noreply.github.com>
2022-12-27 16:38:05 -0600

    nyaa

--- shaders/program/composite/stage_1.glsl
+++ shaders/program/composite/stage_1.glsl
@@ -316,11 +316,11 @@ void main() {
 
     if (info.matMask == 9) {
         vec3 enchantc = 
-        vec3(pow(cellular(((worldPos.xyz)*8.0+vec3(vogelDiskSample(1, 4, interleavedGradientNoise(gl_FragCoord.xy+frameTimeCounter)), 0.0))+frameTimeCounter*0.5), 4.0)*8.0);
+        vec3(pow(cellular(((viewPos.xyz)*8.0+vec3(vogelDiskSample(1, 4, interleavedGradientNoise(gl_FragCoord.xy+frameTimeCounter)), 0.0))+frameTimeCounter*0.5), 4.0)*8.0);
         enchantc.r *= ENCHANT_R;
         enchantc.g *= ENCHANT_G;
         enchantc.b *= ENCHANT_B;
-        enchantc.rgb *= ENCHANT_I;
+        enchantc.rgb *= ENCHANT_I*0.7;
         color = mix(color, mix(enchantc * enchantc, enchantc * color * 64.0, 0.5), luma(enchantc));
     }
 
