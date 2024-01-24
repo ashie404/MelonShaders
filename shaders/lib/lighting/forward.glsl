3931bfac57f8b328d875b716e67e03c5de152718
June <22226164+juniebyte@users.noreply.github.com>
2024-01-23 18:59:00 -0600

    mew

---
+++ shaders/lib/lighting/forward.glsl
@@ -0,0 +1,10 @@
+/*
+    Melon Shaders
+    By Ash (ashie404)
+    https://ashiecorner.xyz
+*/
+
+void GetShading(inout vec3 col, out vec3 shading, vec3 pos_view, vec3 pos_world, vec2 lm, 
+                float emiss, float subsurf) {
+    
+}
